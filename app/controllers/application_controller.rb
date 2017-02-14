0838ff95507bf1d8a7156e4135f1007c87abdda7
SzubiX <szubiwtf@gmail.com>
2017-02-14 17:22:33 +0000

    Dodano przekierowywanie po zalogowaniu dla prowadzacego ktory ma trafic na liste sprawozdan do oceny

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_filter :auth
 
   private
+  	def after_sign_in_path_for(res)
+  		reports_path
+  	end
 	def auth
 		return if controller_name =='reports' && 
 		(action_name =='new' || action_name == 'create')
