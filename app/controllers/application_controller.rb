2bde882177c787796ad30e2b2266e7a0f118d19e
SzubiX <szubiwtf@gmail.com>
2017-02-14 17:19:05 +0000

    Dodano tworzenie sprawozdan bez logowania

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
+  before_filter :auth
+
+  private
+	def auth
+		return if controller_name =='reports' && 
+		(action_name =='new' || action_name == 'create')
+		authenticate_user!
+	end
   protect_from_forgery with: :exception
 end
